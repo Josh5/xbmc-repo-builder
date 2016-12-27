81634ddcb978acd82190291b4251920afff22737
Josh.5 <jsunnex@gmail.com>
2016-12-28 11:30:40 +1300

    Update addons_xml_generator python script. Prevent zipping addons that have not had their versions incremented (saves a fair amount of effort when pushing commits in a git based repo)

--- source/generate_repository.py
+++ source/generate_repository.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-#import addons_xml_generator as generate
 import os
 import shutil
 import time
@@ -21,30 +20,32 @@ def buildrepo(f):
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
-	print "Not Found! Creating: "+d
+    print "Not Found! Creating: "+d
 
 print "-----------------------------------------------------------------------------"
 print "------------------------------------START------------------------------------"
 print "-----------------------------------------------------------------------------"
 
-subprocess.Popen("./addons_xml_generator.py", shell=True, close_fds=True)
 addonxml      = "./addons.xml"
 addonxmlmd5   = "./addons.xml.md5"
 
 for x in os.listdir('./'):
     if (os.path.isfile(x)) or ('.git' in x):
-        #print("SKIPPING: "+x)
+        #### Skip it
         pass
     else: 
         print("Add-on Found: "+x)
         #### Make VARS:
         dest_dir = "../repo/"+x+"/"
-	tree = ET.parse('./'+x+'/addon.xml')
-	root = tree.getroot()
-	version = root.get('version')
+        tree = ET.parse('./'+x+'/addon.xml')
+        root = tree.getroot()
+        version = root.get('version')
         print "Version: "+version
+        isrepo = False
+        if x == 'repository.ember':
+            isrepo = True
         #### DO:
-	buildrepo(dest_dir+"*")
+        buildrepo(dest_dir+"*")
         for file in glob.glob('./'+x+'/*changelog.txt'):
             print 'Copying: '+file+' >>>> '+dest_dir+'changelog-'+version+'.txt'
             shutil.copy(file, dest_dir+'changelog-'+version+'.txt')
@@ -54,15 +55,26 @@ for x in os.listdir('./'):
         for file in glob.glob('./'+x+'/*fanart.*'):
             print 'Copying: '+file+' >>>> '+dest_dir+'fanart.jpg'
             shutil.copy(file, dest_dir)
-        print("Compressing "+x+'-'+version+'.zip...')
-        zipf = zipfile.ZipFile('../repo/'+x+'/'+x+'-'+version+'.zip', 'w', zipfile.ZIP_DEFLATED)
-        zipdir(x, zipf)
-        zipf.close()
+        addon_zip = '../repo/'+x+'/'+x+'-'+version+'.zip'
+        if not os.path.exists(addon_zip):
+            print("Compressing "+x+'-'+version+'.zip...')
+            zipf = zipfile.ZipFile('../repo/'+x+'/'+x+'-'+version+'.zip', 'w', zipfile.ZIP_DEFLATED)
+            zipdir(x, zipf)
+            zipf.close()
+        else:
+            print("!WARNING! Repository already contains "+x+'-'+version+'.zip. You will need to remove this first or increase the addons version number if you wish to overwirte the current version.')
         print("Add-on '"+x+"' Successfully Processed")
         print("")
+        if isrepo:
+            buildrepo("../downloads/*")
+            print("Copy Repository '"+x+"' To Downloads DIR")
+            shutil.copy('../repo/'+x+'/'+x+'-'+version+'.zip', '../downloads/'+x+'.zip')
+            
 
 shutil.copy(addonxml, "../repo/")
 shutil.copy(addonxmlmd5, "../repo/")
+os.remove(addonxml)
+os.remove(addonxmlmd5)
 
 #add version numbers to zip and changelog
 
